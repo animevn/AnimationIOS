307a79564a223f15996fd1a87b4c0d705aaf2988
animevn <haanhth21@gmail.com>
2020-03-12 10:48:07 +0700

    add fadein fadeout fadeoutin animate

--- AnimationIOS/ViewController.swift
+++ AnimationIOS/ViewController.swift
@@ -2,10 +2,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIView!
+    @IBOutlet weak var image: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.layer.borderColor = UIColor.red.cgColor
+        imageView.layer.borderWidth = 0.5
+        imageView.layer.cornerRadius = 10
+    }
+    
+    private func fadeIn(){
+        image.alpha = 0
+        UIView.animate(withDuration: 2, animations: {self.image.alpha = 1})
     }
 
-
+    @IBAction func onFadeInPressed(_ sender: UIButton) {
+        fadeIn()
+    }
+    
+    private func fadeOutIn(){
+        UIView.animate(withDuration: 2, animations: {self.image.alpha = 0}, completion: {_ in
+            UIView.animate(withDuration: 2, animations: {self.image.alpha = 1})
+        })
+    }
+    
+    @IBAction func onFadeOutInPressed(_ sender: UIButton) {
+       fadeOutIn()
+    }
+    
+    private func fadeOut(){
+        UIView.animate(withDuration: 2,
+                       animations: {self.image.alpha = 0},
+                       completion: {_ in self.image.alpha = 1})
+    }
+    
+    @IBAction func onFadeOutPressed(_ sender: UIButton) {
+        fadeOut()
+    }
+    
 }
 
