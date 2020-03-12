c17dd57bda3f2a58450d835b4e1d0ceea74fdaf7
animevn <haanhth21@gmail.com>
2020-03-12 16:10:19 +0700

    add zoom ok

--- AnimationIOS/ViewController.swift
+++ AnimationIOS/ViewController.swift
@@ -28,13 +28,8 @@ class ViewController: UIViewController {
         })
     }
     
-    private func zoomIn(){
-        UIView.animate(withDuration: 2, animations: {
-            self.image.transform = CGAffineTransform(scaleX: 2, y: 2)})
-    }
-    
     @IBAction func onFadeOutInPressed(_ sender: UIButton) {
-       zoomIn()
+       fadeOutIn()
     }
     
     private func fadeOut(){
@@ -46,6 +41,42 @@ class ViewController: UIViewController {
     @IBAction func onFadeOutPressed(_ sender: UIButton) {
         fadeOut()
     }
+    
+    private func zoomIn(){
+        UIView.animate(withDuration: 2, animations: {
+            self.image.transform = CGAffineTransform(scaleX: 2, y: 2)}, completion: {_ in
+                self.image.transform = CGAffineTransform(scaleX: 1, y: 1)
+        })
+    }
+    
+    @IBAction func onZoomInPressed(_ sender: UIButton) {
+        zoomIn()
+    }
+    
+    private func zoomInOut(){
+           UIView.animate(withDuration: 2,
+                          animations: {self.image.transform = CGAffineTransform(scaleX: 2, y: 2)},
+                          completion: {_ in
+                                UIView.animate(withDuration: 2, animations: {
+                                    self.image.transform = CGAffineTransform(scaleX: 1, y: 1)})
+           })
+    }
+    
+    @IBAction func onZoomOutInPressed(_ sender: UIButton) {
+        zoomInOut()
+    }
+    
+    private func zoomOut(){
+        image.transform = CGAffineTransform(scaleX: 2, y: 2)
+        UIView.animate(withDuration: 2, animations: {
+            self.image.transform = CGAffineTransform(scaleX: 1, y: 1)})
+    }
+    
+    @IBAction func onZoomOutPressed(_ sender: UIButton) {
+        zoomOut()
+    }
+    
+    
     
 }
 
