0e3e811f507b8b7db14096fbb5b000239beca80e
animevn <haanhth21@gmail.com>
2020-03-12 17:46:37 +0700

    bounce and flash

--- AnimationIOS/ViewController.swift
+++ AnimationIOS/ViewController.swift
@@ -5,7 +5,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIView!
     @IBOutlet weak var image: UIImageView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.layer.borderColor = UIColor.red.cgColor
@@ -75,6 +74,79 @@ class ViewController: UIViewController {
     @IBAction func onZoomOutPressed(_ sender: UIButton) {
         zoomOut()
     }
+    
+    private func leftIn(){
+        image.transform = CGAffineTransform(translationX: -100, y: 0)
+        UIView.animate(withDuration: 2, animations: {
+            self.image.transform = CGAffineTransform(translationX: 0, y: 0)
+        })
+    }
+    
+    @IBAction func onLeftInPressed(_ sender: UIButton) {
+        leftIn()
+    }
+    
+    private func topBot(){
+        image.transform = CGAffineTransform(translationX: 0, y: -100)
+        UIView.animate(withDuration: 2, animations: {
+            self.image.transform = CGAffineTransform(translationX: 0, y: 0)
+        })
+    }
+    
+    @IBAction func onTopBotPressed(_ sender: UIButton) {
+        topBot()
+    }
+    
+    private func rightIn(){
+        image.transform = CGAffineTransform(translationX: 100, y: 0)
+        UIView.animate(withDuration: 2, animations: {
+            self.image.transform = CGAffineTransform(translationX: 0, y: 0)
+        })
+    }
+    
+    @IBAction func onRightInPressed(_ sender: UIButton) {
+        rightIn()
+    }
+    
+    private func bounce(){
+        UIView.animate(withDuration: 1,animations: {
+            self.image.transform = CGAffineTransform(scaleX: 1, y: 1.5)}, completion: {_ in
+                UIView.animate(withDuration: 1, animations: {
+                    self.image.transform = CGAffineTransform(scaleX: 1, y: 0.5)
+                }, completion: {_ in
+                    UIView.animate(withDuration: 1, animations: {
+                        self.image.transform = CGAffineTransform(scaleX: 1, y: 1)
+                    })
+                })
+        })
+    }
+    
+    @IBAction func onBouncePressed(_ sender: UIButton) {
+        bounce()
+    }
+    
+    private func flash(){
+        var count:Double = 0
+        (0...10).forEach{_ in
+            Timer.scheduledTimer(withTimeInterval: Double(count*0.2), repeats: false, block: {_ in
+                UIView.animate(withDuration: 0.1, animations: {self.image.alpha = 0}, completion: {_ in
+                    UIView.animate(withDuration: 0.1, animations: {self.image.alpha = 1})
+                })
+            })
+            count += 1
+        }
+    }
+    
+    @IBAction func onFlashPressed(_ sender: UIButton) {
+        flash()
+    }
+    
+    @IBAction func onRotateLeftPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func onRotateRightPressed(_ sender: UIButton) {
+    }
+    
     
     
     
