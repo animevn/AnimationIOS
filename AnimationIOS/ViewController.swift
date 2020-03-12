8e9a9a41d9edd577ae9c4b6023130a25390dfa81
animevn <haanhth21@gmail.com>
2020-03-12 10:13:56 +0700

    return to basic ios12

--- AnimationIOS/ViewController.swift
+++ AnimationIOS/ViewController.swift
@@ -1,18 +1,9 @@
-//
-//  ViewController.swift
-//  AnimationIOS
-//
-//  Created by Anh Ha on 3/12/20.
-//  Copyright © 2020 Anh Ha. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
